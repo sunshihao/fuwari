5053356164d1066f52cff6471cc3d5d776a4f24d
sssh <4sh945@outlook.com>
2025-05-20 19:26:57 +0800

    upd: 脚本

--- src/config.ts
+++ src/config.ts
@@ -109,7 +109,7 @@ export const profileConfig: ProfileConfig = {
 		{
 			name: "GitHub",
 			icon: "fa6-brands:github",
-			url: "https://github.com/saicaca/fuwari",
+			url: "https://github.com/thxblog",
 		},
 	],
 };
